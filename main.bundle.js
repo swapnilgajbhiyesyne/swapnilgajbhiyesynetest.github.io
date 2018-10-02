6c319a9570d21a8acfd219b00c62343d6b494289
swapnilgajbhiyesyne <swapnilgajbhiye12@gmail.com>
2018-10-02 22:55:47 +0530

    FIles uploaded for hosting on GitHub

---
+++ main.bundle.js
@@ -0,0 +1,231 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_gendir lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+exports.push([module.i, "@import url(http://fonts.googleapis.com/css?family=Roboto:400);", ""]);
+
+// module
+exports.push([module.i, "body {\r\n  background-color:#fff;\r\n  -webkit-font-smoothing: antialiased;\r\n  font: normal 14px Roboto,arial,sans-serif;\r\n}\r\n\r\n.container {\r\n    padding: 25px;\r\n    position: fixed;\r\n}\r\n\r\n.form-login {\r\n    background-color: #EDEDED;\r\n    padding-top: 10px;\r\n    padding-bottom: 20px;\r\n    padding-left: 20px;\r\n    padding-right: 20px;\r\n    border-radius: 15px;\r\n    border-color:#d2d2d2;\r\n    border-width: 5px;\r\n    box-shadow:0 1px 0 #cfcfcf;\r\n}\r\n\r\nh4 { \r\n border:0 solid #fff; \r\n border-bottom-width:1px;\r\n padding-bottom:10px;\r\n text-align: center;\r\n}\r\n\r\n.form-control {\r\n    border-radius: 10px;\r\n}\r\n\r\n.wrapper {\r\n    text-align: center;\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"well well-lg\">\n\n\n<button type=\"button\" class=\"btn btn-warning\">Vendor</button>\n<button type=\"button\" class=\"btn btn-warning\">Customer</button>\n<button type=\"button\" class=\"btn btn-warning\" (click)=\"sendSMS()\">Send SMS</button>\n\n\n</div>\n<!-- <div class=\"container\">\n  <div class=\"row\">\n      <div class=\"col-md-offset-5 col-md-3\">\n          <div class=\"form-login\">\n          <h4>Welcome back.</h4>\n          <input type=\"text\" id=\"userName\" class=\"form-control input-sm chat-input\" placeholder=\"username\" />\n          \n          <input type=\"text\" id=\"userPassword\" class=\"form-control input-sm chat-input\" placeholder=\"password\" />\n          \n          <div class=\"wrapper\">\n          <span class=\"group-btn\">     \n              <a href=\"#\" class=\"btn btn-primary btn-md\">login <i class=\"fa fa-sign-in\"></i></a>\n          </span>\n          </div>\n          </div>\n      \n      </div>\n  </div>\n</div> -->\n\n<div class=\"container\" *ngIf=\"show\">\n  <h1>Book Form</h1>\n  <form #frm=\"ngForm\" (ngSubmit)=\"submitForm(frm.value)\">\n    <div class=\"form-group\">\n      <label for=\"title\">Title</label>\n      <input type=\"text\" class=\"form-control\" id=\"title\" required [(ngModel)]=\"title\" name=\"title\">\n    </div>\n    <div class=\"form-group\">\n      <label for=\"author\">Author</label>\n      <input type=\"text\" class=\"form-control\" id=\"author\" required [(ngModel)]=\"author\" name=\"author\">\n    </div>\n    <div class=\"form-group\">\n      <label for=\"url\">URL</label>\n      <input type=\"text\" class=\"form-control\" id=\"url\" [(ngModel)]=\"url\" name=\"url\">\n    </div>\n    <button type=\"submit\" class=\"btn btn-default\">Submit</button>\n   </form>\n </div>\n\n\n  <!-- Send SMS Form -->\n  <div class=\"container\" *ngIf=\"showSMSForm\">\n    <h1>Send SMS</h1>\n    <form #smsfrm=\"ngForm\" (ngSubmit)=\"sendSMSsubmitForm(smsfrm.value)\">\n      <div class=\"form-group\">\n        <label for=\"title\">Recepient Mobile No</label>\n        <input type=\"text\" class=\"form-control\" id=\"to\" required [(ngModel)]=\"to\" name=\"to\">\n      </div>\n      <div class=\"form-group\">\n        <label for=\"author\">Message</label>\n        <input type=\"text\" class=\"form-control\" id=\"message\" required [(ngModel)]=\"message\" name=\"message\">\n      </div>\n      \n      <button type=\"submit\" class=\"btn btn-default\">Submit</button>\n     </form>\n   </div>\n\n   <!-- CUstomer Registration -->\n   <form [formGroup]=\"form\" (ngSubmit)=\"onSubmit()\">\n    <div class=\"form-group\">\n      <label for=\"name\">Name</label>\n      <input type=\"text\" class=\"form-control\" id=\"name\" placeholder=\"Bob\" formControlName=\"name\">\n    </div>\n    <div class=\"form-group\">\n      <label for=\"avatar\">Upload Aadhar</label>\n      <input type=\"file\" id=\"avatar\" (change)=\"onFileChange($event)\" #fileInput>\n      <label for=\"avatar\">Upload Aadhar</label>\n      <input type=\"file\" id=\"avatar\" (change)=\"onFileChange($event)\" #fileInput>\n      <button type=\"button\" class=\"btn btn-sm btn-default\" (click)=\"clearFile()\">clear file</button>\n    </div>\n    <button type=\"submit\" [disabled]=\"form.invalid || loading\" class=\"btn btn-success\">Submit <i class=\"fa fa-spinner fa-spin fa-fw\" *ngIf=\"loading\"></i></button>\n  </form>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common_http__ = __webpack_require__("../../../common/@angular/common/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var AppComponent = (function () {
+    function AppComponent(http, fb) {
+        this.http = http;
+        this.fb = fb;
+        this.title = 'app';
+        this.show = false;
+        this.showSMSForm = false;
+        this.smsUrl = "https://smsapi.engineeringtgr.com/send/?Mobile";
+        this.form = this.fb.group({
+            name: ['', __WEBPACK_IMPORTED_MODULE_2__angular_forms__["d" /* Validators */].required],
+            avatar: null
+        });
+    }
+    AppComponent.prototype.submitForm = function (frm) {
+        console.log(frm);
+    };
+    AppComponent.prototype.sendSMS = function () {
+        this.showSMSForm = true;
+        this.show = false;
+    };
+    AppComponent.prototype.sendSMSsubmitForm = function (smsfrm) {
+        console.log(smsfrm.to);
+        this.smsUrl = "https://smsapi.engineeringtgr.com/send/?Mobile=9096660070&Password=realway9090&Key=swapnvlL4uZobhspFKqnm&Message=" + smsfrm.message + "&To=" + smsfrm.to + "";
+        console.log(this.smsUrl);
+        var status;
+        this.http.get(this.smsUrl).subscribe(function (successCode) {
+            status = successCode;
+        }, function (errorCode) { return status = errorCode; });
+    };
+    AppComponent.prototype.clearFile = function () {
+        this.form.get('avatar').setValue(null);
+        this.fileInput.nativeElement.value = '';
+    };
+    AppComponent.prototype.onSubmit = function () {
+        var formModel = this.form.value;
+        console.log(formModel);
+    };
+    AppComponent.prototype.onFileChange = function (event) {
+        var _this = this;
+        var reader = new FileReader();
+        if (event.target.files && event.target.files.length > 0) {
+            var file_1 = event.target.files[0];
+            reader.readAsDataURL(file_1);
+            reader.onload = function () {
+                _this.form.get('avatar').setValue({
+                    filename: file_1.name,
+                    filetype: file_1.type,
+                    value: reader.result.split(',')[1]
+                });
+            };
+        }
+    };
+    return AppComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_13" /* ViewChild */])('fileInput'),
+    __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* ElementRef */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* ElementRef */]) === "function" && _a || Object)
+], AppComponent.prototype, "fileInput", void 0);
+AppComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormBuilder */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormBuilder */]) === "function" && _c || Object])
+], AppComponent);
+
+var _a, _b, _c;
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_common_http__ = __webpack_require__("../../../common/@angular/common/http.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_forms__["b" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_4__angular_common_http__["b" /* HttpClientModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_forms__["c" /* ReactiveFormsModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_20" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
